2eee00c9f0d3e989d92e60d5cb0d7051c92ddb0c
zordking <zordkingking@yandex.ru>
2015-11-19 00:00:52 +0200

    Init commit.

---
+++ db/guidelines.sql
@@ -0,0 +1,12 @@
+
+Project Developed by Sanjeev sir's Student
+any query regarding project 
+
+
+create databse name "full"
+admin login: sanjeev
+	sanjeev
+
+
+contact at me phptpoint@gmail.com
+mobile: 9015501897
